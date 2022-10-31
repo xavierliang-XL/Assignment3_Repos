4e4a6421cf2127e93ef5ad8bcbf13d180c1034ba
Xavier <2762507342@qq.com>
2022-10-31 01:08:28 -0400

    select and get completed

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-let response = axios.get("https://api.themoviedb.org/3/search/movie", {
-  params: {
-    api_key: "23b3a0cee96fcac58b28918686474f75",
-    include_adult: "false",
-    query: "batman",
-  }
-});
-
-response = response.then((moviesData) => {
-  for (let movie of moviesData.data.results) {   
-    axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-      params: {
-        api_key: "23b3a0cee96fcac58b28918686474f75",
-        append_to_response: "videos",
-      }
-    }).then((movieData) => {
-      const img = document.createElement('img');
-      const p = document.createElement('p');
-      const iframe = document.createElement('iframe');
-
-      const trailers = movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
-      iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
-      img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      p.innerHTML = `${movie.title} -- ${movie.release_date} -- ${movie.popularity}`;
-      
-      document.body.append(p);
-      document.body.append(img);
-      document.body.append(iframe);
+const btn = document.getElementById("search_btn");
+let remove_list= [];
+btn.addEventListener("click", function () {
+    console.log(remove_list.length);
+    for(let i of remove_list){
+        i.remove();
+    }
+    var keyword = document.getElementById("movies");
+    if(keyword.value == "None"){
+        return;
+    }
+    let response = axios.get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+            api_key: "23b3a0cee96fcac58b28918686474f75",
+            include_adult: "false",
+            query: keyword.value,
+        }
     });
-  }
+
+    response = response.then((moviesData) => {
+    for (let movie of moviesData.data.results) {   
+            axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
+            params: {
+                api_key: "23b3a0cee96fcac58b28918686474f75",
+                append_to_response: "videos",
+            }
+        }).then((movieData) => {
+            const img = document.createElement('img');
+            const p = document.createElement('p');
+            const iframe = document.createElement('iframe');
+            remove_list.push(p,iframe,img);
+            const trailers = movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
+            try{
+                iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
+          
+                }catch(err){
+                  return;
+                }
+                img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+                p.innerHTML = `${movie.title} -- ${movie.release_date} -- ${movie.popularity}`;
+                
+                document.body.append(p,img,iframe);
+              });
+        }
+    });
 });
